1b446dfa68a0c6493aebf01d50db27c4da695bb7
=azuryk <=airsoftpierro@gmail.com>
2018-05-14 10:58:34 +0200

    creation de la page de deconnexion

---
+++ deconnexion.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+
+unset($_SESSION['user']);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Déconnexion</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+    <p>Vous avez bien été déconnecté !</p>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+
+</body>
+
+</html>
